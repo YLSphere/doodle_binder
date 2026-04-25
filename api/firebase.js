d584b8c6950a289bc4ecb61e829c45ebe0fcf668
YLSphere <lai.myron@gmail.com>
2026-04-25 01:36:37 -0700

    change firebase js

--- api/firebase.js
+++ api/firebase.js
@@ -1,10 +1,15 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error('Missing FIREBASE_SERVICE_ACCOUNT env var');
+  }
+
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    projectId: serviceAccount.project_id,
   });
 }
 
